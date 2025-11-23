3d3c5fce8a7e8fccbf47129a4956963b3f4105c9
SaikoPsyche <87397079+SaikoPsyche@users.noreply.github.com>
2025-11-23 15:48:10 -0500

    Kinda works now
    Update

--- fish.py
+++ fish.py
@@ -2,12 +2,12 @@ import pygame
 from setup import *
 
 class Bird:
-    def __init__(self, bird_img, x, y):
+    def __init__(self, x, y, sprite):
         self.x = x
         self.y = y
         self.vel = 0
-        self.image = bird_img
-        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.sprite = sprite
+        self.rect = self.sprite.get_rect(center=(self.x, self.y))
         self.alive = True
 
     def flap(self):
@@ -19,6 +19,6 @@ class Bird:
         self.rect.centery = self.y
 
     def draw(self, surf):
-        rotated = pygame.transform.rotate(self.image, -self.vel * 3)
+        rotated = pygame.transform.rotate(self.sprite, -self.vel * 3)
         rect = rotated.get_rect(center=(self.x, self.y))
         surf.blit(rotated, rect)
--- pipe.py
+++ pipe.py
@@ -2,14 +2,13 @@ import pygame
 from setup import *
 
 class Pipe:
-    def __init__(self, pipe_img, x, gap_y):
+    def __init__(self, x, gap_y, sprite_top, sprite_bottom):
         self.x = x
         self.gap_y = gap_y
+        self.sprite_top = sprite_top
+        self.sprite_bottom = sprite_bottom
+        self.width = sprite_top.get_width()
         self.passed = False
-        self.width = pipe_img.get_width()
-        self.height = pipe_img.get_height()
-        self.top_img = pygame.transform.flip(pipe_img, False, True)
-        self.bottom_img = pipe_img
 
     def update(self):
         self.x -= PIPE_SPEED
@@ -17,14 +16,16 @@ class Pipe:
     def off_screen(self):
         return self.x + self.width < 0
 
-    def draw(self, surf):
-        top_rect = self.top_img.get_rect(midbottom=(self.x + self.width // 2, self.gap_y - PIPE_GAP_SIZE // 2))
-        bottom_rect = self.bottom_img.get_rect(midtop=(self.x + self.width // 2, self.gap_y + PIPE_GAP_SIZE // 2))
-        surf.blit(self.top_img, top_rect)
-        surf.blit(self.bottom_img, bottom_rect)
-
     def top_rect(self):
-        return self.top_img.get_rect(midbottom=(self.x + self.width // 2, self.gap_y - PIPE_GAP_SIZE // 2))
+        return self.sprite_top.get_rect(
+            midbottom=(self.x + self.width // 2, self.gap_y - PIPE_GAP_SIZE // 2)
+        )
 
     def bottom_rect(self):
-        return self.bottom_img.get_rect(midtop=(self.x + self.width // 2, self.gap_y + PIPE_GAP_SIZE // 2))
+        return self.sprite_bottom.get_rect(
+            midtop=(self.x + self.width // 2, self.gap_y + PIPE_GAP_SIZE // 2)
+        )
+
+    def draw(self, surf):
+        surf.blit(self.sprite_top, self.top_rect())
+        surf.blit(self.sprite_bottom, self.bottom_rect())
